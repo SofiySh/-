f294552266a96022b90d0e8da9c2256b7d6ab764
SofiySh <proskurnina.sofy@yandex.ru>
2024-06-02 11:46:17 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,14 @@
+print ('Строки и индексация строк')
+example = "Топинамбур"
+print(example [0])
+print(example [-1])
+print(example [5:10])
+print(example [::-1])
+print(example [0:10:2])
+
+a = "Электрификация"
+print(a [0])
+print(a [-1])
+print(a [8:14])
+print(a [::-1])
+print(a [0:14:2])
